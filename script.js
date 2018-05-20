78f57456d41aa5d1c13d00efb9c933328ff590c7
AdityaGupta1 <adityag@gmail.com>
2018-05-19 17:45:35 -0700

    can now sign up and remove sign up for events

--- script.js
+++ script.js
@@ -8,22 +8,65 @@ var config = {
 };
 firebase.initializeApp(config);
 
-const database = firebase.firestore();
-const settings = {/* your settings... */ timestampsInSnapshots: true};
-database.settings(settings);
+const db = firebase.firestore();
+db.settings({ timestampsInSnapshots: true });
 
 var taskString = "";
 
-database.collection("tasks").get().then(function(querySnapshot) {
-    querySnapshot.forEach(function(doc) {
-        var task = doc.data();
-        taskString += "<h3>" + task.name + " (" + task.leader + ")</h3>"
-                + "<table class='table-striped'>";
-        for (var i in task.participants) {
-            taskString += "<tr><td>" + task.participants[i] + "</td></tr>";
-        }
-        taskString += "</table><br>";
+// will get actual signed in user once it's on wordpress
+const user = "Aditya Gupta";
+
+load();
+
+function load() {
+    $("#tasks").html("Loading...");
+    taskString = "";
+
+    db.collection("tasks").get().then(function(querySnapshot) {
+        querySnapshot.forEach(function(doc) {
+            var task = doc.data();
+            taskString += "<h3>" + task.name + " (" + task.leader + ")";
+            var participants = task.participants;
+
+            if (participants.length < task.max_participants) {
+                taskString += "<button style='margin-left:7px;' onclick='signup(\"" + doc.id + "\")'>+</button>";
+            }
+
+            taskString += "</h3><table class='table-striped'>";
+
+            for (var i in participants) {
+                var participant = participants[i];
+                taskString += "<tr><td>" + participant;
+                if (participant === user) {
+                    taskString += "<button style='margin-left:7px;' onclick='removeSignup(\"" + doc.id + "\")'>-</button>";
+                }
+                taskString += "</td></tr>";
+            }
+            taskString += "</table><br>";
+        });
+    }).then(function() {
+        $("#tasks").html(taskString);
     });
-}).then(function() {
-    $("#tasks").html(taskString);
-});
+}
+
+function signup(id) {
+    var task = db.collection("tasks").doc(id);
+    task.get().then(function(doc) {
+        var participants = doc.data().participants;
+        participants.push(user);
+        task.update({ "participants": participants });
+    }).then(function() {
+        load();
+    });
+}
+
+function removeSignup(id) {
+    var task = db.collection("tasks").doc(id);
+    task.get().then(function(doc) {
+        var participants = doc.data().participants;
+        participants.splice(participants.indexOf(user), 1);
+        task.update({ "participants": participants });
+    }).then(function() {
+        load();
+    });
+}
